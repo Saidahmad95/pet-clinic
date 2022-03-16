368ec72c0698ac2d6b7bad11bfde9bc478f5e1d7
Saidahmad95 <murtazoyev00@gmail.com>
2022-03-16 03:50:35 +0000

    third step. Switch to multi module maven project.

--- pet-clonic-web/src/main/java/com/example/PetClinicApplication.java
+++ pet-clonic-web/src/main/java/com/example/PetClinicApplication.java
@@ -1,11 +1,10 @@
-package com.example.petclinic;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetClinicApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
     }
